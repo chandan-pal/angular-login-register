e13f6e472d036b93b197859c105c03fc2d8a9967
chandan-pal <chandanpal290190@gmail.com>
2020-06-21 00:12:05 +0530

    import forms module

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [RegisterComponent, LoginComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthenticationModule { }
