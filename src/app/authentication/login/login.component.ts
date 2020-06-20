ed45c63c8879843867bad408e60efd3c7036164d
chandan-pal <chandanpal290190@gmail.com>
2020-06-21 02:24:15 +0530

    implement login method

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms'
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,11 +11,13 @@ import { FormGroup, Validators, FormBuilder } from '@angular/forms'
 export class LoginComponent implements OnInit {
 
   public overlayDisplay = false;
-	public loginError = true;
+	public loginError = false;
 
 	public loginForm: FormGroup;
 
   constructor(
+    private router: Router,
+    private authservice: AuthService
 	) {
     this.loginForm = new FormBuilder().group({
 			email: ['', Validators.compose([Validators.required, Validators.email])],
@@ -27,6 +31,28 @@ export class LoginComponent implements OnInit {
 
   onSubmit(): void {
 
+    // if form is not valid do nothing
+    if (this.loginForm.invalid)
+    {
+      return;
+    }
+
+    // set loading
+    this.overlayDisplay = true;
+    this.loginError =  false;
+
+    this.authservice.login(this.loginForm.controls.email.value, this.loginForm.controls.password.value)
+          .subscribe(
+            (data) => {
+              this.overlayDisplay = false;
+              localStorage.setItem('userid', data.userId);
+              this.router.navigate(['home']);
+            },
+            (error) => {
+              this.overlayDisplay = false;
+              this.loginError =  true;
+            }
+          )
   }
 
 }
--- src/app/authentication/services/auth.service.ts
+++ src/app/authentication/services/auth.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private http: HttpClient) { }
+
+    login(email: string, password: string) {
+
+      const httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type':  'application/json',
+          'Access-Control-Allow-Origin': '*'
+        })
+      };
+      return this.http.post<User>(environment.authUrl, {"userEmail": email, "password": password}, httpOptions)
+
+    }
 }
