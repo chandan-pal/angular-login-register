882970e5643d97d7e04d1c5aaf2cca8010575cc4
chandan-pal <chandanpal290190@gmail.com>
2020-06-20 17:16:23 +0530

    move login component to authentication module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     HomeComponent
   ],
   imports: [
--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
-  declarations: [RegisterComponent],
+  declarations: [RegisterComponent, LoginComponent],
   imports: [
     CommonModule,
     AuthenticationRoutingModule
