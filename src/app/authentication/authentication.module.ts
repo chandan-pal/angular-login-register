6f746b18fc2fa13fd872dd394a7b61465e1c9de1
chandan-pal <chandanpal290190@gmail.com>
2020-06-20 17:13:33 +0530

    move registration component to authentication module

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthenticationRoutingModule } from './authentication-routing.module';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [RegisterComponent],
   imports: [
     CommonModule,
     AuthenticationRoutingModule
