cd70d23ab970499f6aa2fd7c6edcd85b5ecf44b3
chandan-pal <chandanpal290190@gmail.com>
2020-06-21 03:42:15 +0530

    complete the logic of is auth-guard to allow if user is logged in

--- src/app/AuthGuard/auth.guard.ts
+++ src/app/AuthGuard/auth.guard.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '../authentication/services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(public router: Router) {}
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if (this.authService.isLoggedIn())
+      {
+        return true;
+      }
 
       this.router.navigate(["login"]);
       return false;
