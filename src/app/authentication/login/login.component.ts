3701e37916f390272acd204de132bbc78f8a4d72
chandan-pal <chandanpal290190@gmail.com>
2020-06-21 00:13:46 +0530

    design login form

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms'
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public overlayDisplay = false;
+	public loginError = true;
+
+	public loginForm: FormGroup;
+
+  constructor(
+	) {
+    this.loginForm = new FormBuilder().group({
+			email: ['', Validators.compose([Validators.required, Validators.email])],
+			password: ['', Validators.required],
+		});
+	}
 
   ngOnInit(): void {
+    //this.overlayDisplay = true;
+  }
+
+  onSubmit(): void {
+
   }
 
 }
