02fad11ba7f1302091a7369b0eb6c23a2212d4e1
chandan-pal <chandanpal290190@gmail.com>
2020-06-21 15:35:25 +0530

    implement logout functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './authentication/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-login-register';
+  isLoggedIn = false;
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.isLoggedIn = this.authService.isLoggedIn();
+  }
+
+  ngDoCheck() {
+    this.isLoggedIn = this.authService.isLoggedIn();
+  }
+
+  logout() {
+    this.authService.logout();
+    this.isLoggedIn = false;
+  }
 }
