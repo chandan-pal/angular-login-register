f06bf6e2cbbb20e1cbcc9a3dfceafb5e191456f1
chandan-pal <chandanpal290190@gmail.com>
2020-06-21 12:29:23 +0530

    design registration form

--- src/app/authentication/register/register.component.ts
+++ src/app/authentication/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public loading = false;
+  public submitted = false;
+  public registrationForm: FormGroup;
+
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
+    this.registrationForm = this.fb.group(
+      {
+        firstName: ['', Validators.required],
+        lastName: ['', ''],
+        email: ['', Validators.compose([Validators.required, Validators.email])],
+			  password: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
+      }
+    )
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.registrationForm.invalid)
+    {
+      return;
+    }
+    this.loading = true;
+
   }
 
 }
