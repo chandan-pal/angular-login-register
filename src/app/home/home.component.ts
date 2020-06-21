a75271949de3a29e2172295866b5e0f8449eb425
chandan-pal <chandanpal290190@gmail.com>
2020-06-21 14:31:31 +0530

    display user name in home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  public user;
   constructor() { }
 
   ngOnInit(): void {
+    this.user = JSON.parse(localStorage.getItem('user'))
   }
 
 }
