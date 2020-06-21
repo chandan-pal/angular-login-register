847ecef33eeb3bbed1960bed7c979ffeec4c2652
chandan-pal <chandanpal290190@gmail.com>
2020-06-21 13:35:33 +0530

    added registration url in environment variable

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  authUrl: "http://127.0.0.1:8080/authenticate"
+  authUrl: "http://127.0.0.1:8080/authenticate",
+  registrationUrl: "http://127.0.0.1:8080/register"
 };
 
 /*
